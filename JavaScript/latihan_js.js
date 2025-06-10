f25a1ed24c8a3cadf6ad48c96d857265b66e2336
Sulu Edward Julianto <sulujulianto@gmail.com>
2025-06-10 09:59:07 +0700

    Add files via upload

---
+++ JavaScript/latihan_js.js
@@ -0,0 +1,22 @@
+document.writeln("<PRE>");
+document.write('<p><B><FONT SIZE=3 COLOR="black">');
+document.writeln("Contoh ini mengunakan Javascript yang diambil dari file lain.");
+document.write("</FONT></B></p>");
+
+document.writeln("<hr>");
+
+document.write('<center><B><FONT size=7 COLOR="black">');
+document.writeln("UNIVERSITAS KOMPUTER INDONESIA");
+document.write("</FONT></B></center></br>");
+
+document.write('<center><B><FONT size=6 COLOR="black">');
+document.writeln("FAKULTAS TEKNIK dan ILMU KOMPUTER");
+document.write("</FONT></B></center></br>");
+                
+document.write('<center><B><FONT size=5 COLOR="black">');
+document.writeln("JURUSAN TEKNIK INFORMATIKA");
+document.write("</FONT></B></center></br>");
+                
+document.writeln("<hr>");
+
+document.writeln("</PRE>");
