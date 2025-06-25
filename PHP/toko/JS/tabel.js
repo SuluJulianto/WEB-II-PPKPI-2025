c1ac5a5469dd59bce5696ecc106295ce81cee579
Sulu Edward Julianto <sulujulianto@gmail.com>
2025-06-25 14:13:23 +0700

    Update commit

---
+++ PHP/toko/JS/tabel.js
@@ -0,0 +1,15 @@
+function btn_edit(row_id, row_product){
+    console.log('edit: '+row_id);
+    alert('Edit: ' +row_id +' -> '+ row_product);
+}
+
+function btn_delete(row_id, row_product){
+    console.log('delete: '+row_id);
+    alert('Delete: ' + row_id +' -> ' + row_product);
+}
+
+$(document).ready(function(){
+
+
+    
+});
