63afa60fa96c0836048fb38cc6ba0f6610ec1cba
Sulu Edward Julianto <sulujulianto@gmail.com>
2025-06-25 14:16:13 +0700

    Update index.php

--- PHP/toko/public/index.php
+++ PHP/toko/public/index.php
@@ -1,20 +1,25 @@
 <?php
-    if (!isset($_SESSION)){
-        session_start();
-    }
-if (!isset($_SESSION["dir_root"])){
-    require_once 'boostrap.php';
+if (!isset($_SESSION)) {
+    session_start();
+}
+if (!isset($_SESSION["dir_root"])) {
+    require_once 'bootstrap.php';
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <a href="tabel.php">Tabel</a>
+    <a href="pageSales.php">Sales</a>
 </body>
+
 </html>
